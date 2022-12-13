6523ad56e328a96992a93d95f5169c70f3145f46
Sameer Misbah <sameermisbah18@gmail.com>
2022-12-13 09:32:26 +0500

    Recreated a new repo for this app

---
+++ README.md
@@ -0,0 +1,45 @@
+# Docker swarm visualizer
+
+A docker swarm cluster visualizer application. You can run it in container cluster and use as a dashboard to visualize the service-task distributions among nodes in realtime.
+## Screenshot  - Nodes
+![alt text](http://i.imgur.com/tPvbbJt.gif)
+## Screenshot  - Tasks
+![alt text](https://raw.githubusercontent.com/MoimHossain/docker-viswarm/master/screenshots/tasks.png)
+
+## How to run
+
+To get the visualizer up and running in  your cluster
+```
+> docker run -p 9009:9009 -v /var/run/docker.sock:/var/run/docker.sock moimhossain/viswarm
+```
+
+## How to develop
+
+This is what you need to do in order to start hacking the codebase
+
+```
+> npm install
+```
+
+It might be the case that you have installed webpack locally and received a __webpack command not recognized__ error. In such scenarios, you can either install webpack globally or run it from local installation by using the following command:
+
+```
+node_modules\.bin\webpack
+```
+
+Start the development server (changes will now update live in browser)
+```
+> npm run dev
+```
+
+To view your project, go to: [http://localhost:9009/](http://localhost:9009/)
+
+
+## Info
+This application was built with React JS, Redux, Docker Remote API. Therefore, you need to be familiar with these concepts.
+
+### Contribution and license
+
+MIT licensed, and open for anything with no obligation. Feel free to use/customize/change it anyway you see fit.
+Giving me a thumbs up, a token of appreciation will make me very happy!
+
---
+++ server.js
@@ -0,0 +1,105 @@
+
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var Docker = require('dockerode');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('src'))
+
+var port = process.env.PORT || 9009;        // set our port
+
+var router = express.Router();              // get an instance of the express Router
+
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.get('/containers', function(req, res) {
+    
+    var docker = new Docker();
+    docker.listContainers({all: false}, function(err, containers) {
+        res.json(containers);
+    });
+});
+router.get('/images', function(req, res) {
+    
+    var docker = new Docker();
+    docker.listImages({all: false}, function(err, images) {
+        res.json(images);
+    });
+});
+
+
+router.get('/networks', function(req, res) {
+    
+    var docker = new Docker();
+    docker.listNetworks({all: false}, function(err, networks) {
+        res.json(networks);
+    });
+});
+router.get('/swarm', function(req, res) {
+    
+    var docker = new Docker();
+   
+    docker.swarmInspect(function(err, swarnInfo) {
+        res.json(swarnInfo);
+    });
+});
+
+router.get('/nodes', function(req, res) {
+    
+    var docker = new Docker();
+   
+    docker.listNodes(function(err, nodes) {
+        res.json(nodes);
+    });
+});
+
+router.delete('/nodes/:nodeId', function(req, res) {
+    if(req.params.nodeId) {
+        console.log('Request DELETE NODE ID=' + req.params.nodeId)
+        var docker = new Docker();
+        docker.listNodes({all: true}, function(err, nodes) {
+            nodes.forEach((node) => {                
+                if(node.ID === req.params.nodeId) {
+                    node.remove({force: true}, function(nres) {
+                        res.json(nres);
+                    });
+                }
+            });
+        });
+    } else {
+        res.json({ message: 'Invalid operation.' });
+    }
+});
+
+router.get('/services', function(req, res) {
+    
+    var docker = new Docker();
+   
+    docker.listServices(function(err, nodes) {
+        res.json(nodes);
+    });
+});
+router.get('/tasks', function(req, res) {
+    
+    var docker = new Docker();
+   
+    docker.listTasks(function(err, nodes) {
+        res.json(nodes);
+    });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
